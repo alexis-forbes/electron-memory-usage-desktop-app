cd222beaf6cf71caa976d54057766ab11ed7266a
alexis-forbes <a.forbes.gil@gmail.com>
2025-11-14 13:44:03 +0100

    feat: add e2e tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,59 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, _electron } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+let electronApp: Awaited<ReturnType<typeof _electron.launch>>;
+let mainPage: Awaited<ReturnType<typeof electronApp.firstWindow>>;
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+// avoids race condition with preload script
+// race condition is that the preload script may not be loaded before the test tries to use it
+async function waitForPreloadScript() {
+  return new Promise((resolve) => {
+    const interval = setInterval(async () => {
+     const electronBridge = await mainPage.evaluate(() => {
+        // Playwright does not use the tsconfig so we need to cast window
+        // we get the electron object created in the preload script in main.ts preload
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (window as Window & { electron?: any }).electron;
+      });
+      if (electronBridge) {
+        // stop polling and resolve the promise
+        clearInterval(interval);
+        resolve(true);
+      }
+    }, 100);
+  });
+}
+
+test.beforeEach(async () => {
+  electronApp = await _electron.launch({
+    args: ['.'],
+    env: { NODE_ENV: 'development' },
+  });
+  mainPage = await electronApp.firstWindow();
+  await waitForPreloadScript();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test.afterEach(async () => {
+  await electronApp.close();
 });
+
+test('Custom frame should minimize the mainWindow', async () => {
+  await mainPage.click('#minimize')
+  const isMinimized = await electronApp.evaluate((electron) => {
+    // Test that the app is still running after minimize
+    return electron.BrowserWindow.getAllWindows()[0].isMinimized();
+  });
+  expect(isMinimized).toBeTruthy();
+})
+
+test('should create a custom menu', async () => {
+  // Test that the app creates a custom menu
+  const menu = await electronApp.evaluate((electron) => {
+    return electron.Menu.getApplicationMenu();
+  });
+  expect(menu).not.toBeNull();
+  expect(menu).toBeDefined();
+  expect(menu?.items).toHaveLength(2);
+  expect(menu?.items[0].submenu?.items).toHaveLength(2);
+  expect(menu?.items[1].submenu?.items).toHaveLength(3);
+  expect(menu?.items[1].label).toBe('View');
+})
